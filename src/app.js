e082502fb1e905ffe7e483aee0207905e0326280
tinhbe <98801099+tvantinh@users.noreply.github.com>
2024-10-16 09:49:16 +0700

    firebaseAuthentication

--- src/app.js
+++ src/app.js
@@ -9,6 +9,31 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCwq7W_CVOsgGuK-pWuBiVUfWQ_9mmaxdQ",
+  authDomain: "e-commercewebsite-645fb.firebaseapp.com",
+  projectId: "e-commercewebsite-645fb",
+  storageBucket: "e-commercewebsite-645fb.appspot.com",
+  messagingSenderId: "682014935245",
+  appId: "1:682014935245:web:3459224b3ea8e33c040184",
+  measurementId: "G-4F29WG76RC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
 (async () => {
   try {
     await sequelize.authenticate();
