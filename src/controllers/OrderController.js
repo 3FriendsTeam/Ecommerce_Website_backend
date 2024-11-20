33db6a0e3723c1a0960f378eee44b36d7a1e7467
tinhbe <98801099+tvantinh@users.noreply.github.com>
2024-11-20 18:18:19 +0700

    update

--- src/controllers/OrderController.js
+++ src/controllers/OrderController.js
@@ -73,7 +73,7 @@ const getShipingOrders = async (req, res) => {
                 "CustomerID"
             ],
             where: {
-                OrderStatus: "Chờ giao hàng",
+                OrderStatus: "Chờ giao hàng" || "Đang giao",
             }
         });
 
@@ -99,7 +99,7 @@ const getPackingOrders = async (req, res) => {
                 "CustomerID"
             ],
             where: {
-                OrderStatus: "Đã xác nhận",
+                OrderStatus: "Đã xác nhận" || "Đang đóng hàng",
             }
         });
 
